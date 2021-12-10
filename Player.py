26b102e5e3fd4cf02c23c07a25461dd53748a90b
NitroSniper <NitroSniper@users.noreply.github.com>
2021-12-10 02:39:23 +0000

    Created Game Loop and Player Prototype
    Created a Game Loop which is contains Player Events, Player Update Logic
    And Player Display
    Dt is also Implemented and HDMode is also included.
    
    A prototype Player has been made which is just a square with
    basic parameters which includes custom key controls.

--- Player.py
+++ Player.py
@@ -1,4 +1,26 @@
+import pygame
 class PlayerObject(object):
     def __init__(self, keys):
         '''DOCSTRING'''
-        self.keys = keys
+        self.keys = keys #<show> Player Control Key
+        self.movements = {
+            'UP' : False,
+            'DOWN' : False,
+            'LEFT' : False,
+            'RIGHT' : False
+        }
+        self.x = 200 #<show> Player x coordinate
+        self.y = 300 #<show> Player y coordinate
+        self.image = pygame.surface.Surface((30, 30)) #<show> Player image
+        self.image.fill((0,0,255))
+        self.speed = 3 #<show> Player movement speed
+
+    def update(self, dt):
+        if self.movements['UP']: #<show> if movement 'up' is True
+            self.y -= self.speed*dt #<show> Player moves upwards
+        if self.movements['DOWN']: #<show> if movement 'down' is True
+            self.y += self.speed*dt #<show> Player moves downwards
+        if self.movements['LEFT']: #<show> if movement 'left' is True
+            self.x += self.speed*dt #<show> Player moves to the left
+        if self.movements['RIGHT']: #<show> if movement 'right' is True
+            self.x -= self.speed*dt #<show> Player moves to the right
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from Player import PlayerObject
 from time import perf_counter
 from pygame.locals import (
     QUIT,
@@ -11,31 +12,57 @@ from pygame.locals import (
 )
 pygame.init()
 
-
+#
 #Window Stuff
-FPS = 120
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
+FPS = 60 #<show> Base Frames Per Second of the Game
+SCREEN_WIDTH = 600 #<show> pixel Width of the game
+SCREEN_HEIGHT = 400 #<show> pixel height of the game
 WINDOW_SIZE = (900, 600) #x, y
-pygame.display.set_caption("Template") 
+pygame.display.set_caption("Template") #<show> Sets the caption of pygame window
 #Set the Caption Window Like 'Terraria: Also Try Minecraft'
 DISPLAY = pygame.display.set_mode(WINDOW_SIZE, 0, 32) #True Screen
 SCREEN = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT)) #Screen to Blit on other Screen
-HDMode = True
+HDMode = True #<show> whether  
 
+P1 = PlayerObject((K_w, K_s, K_d, K_a))#<show> Object of Player 1
 
-def Game():
-    last_dt = perf_counter()
-    program_running = True
-    while program_running:
-        dt = (perf_counter() - last_dt)*FPS
-        last_dt = perf_counter()
-        for event in pygame.event.get():
+def Game(): #<show> Call Function of the game  
+    PLAYERS = [P1] #<show> Holds the List of Players
+    last_dt = perf_counter() #<show> last Game Loop time
+    program_running = True #<show> Boolean thattells if the game is running or not
+    while program_running: #<show> While Game is running
+        dt = (perf_counter() - last_dt)*FPS #<show> calculate the movement multiplier for it to be time dependent
+        last_dt = perf_counter() #<show> last Game Loop time
+
+        for event in pygame.event.get(): #<show> for new events happening
+            #Start Code Block (can be simplified)
             if event.type == KEYDOWN: #if a key is pressed down
-                pass
-
+                for player in PLAYERS: #<show> for all players in PLAYERS
+                    for direction, key in zip(player.movements, player.keys): #<show> get all directions (UP, DOWN, LEFT, RIGHT)
+                        if event.key == key: #<show> if new event is player control key
+                            player.movements[direction] = True #<show> set direction to True
             elif event.type == KEYUP: # if a key is pressed up
-                pass
-
-            elif event.type == QUIT:
-                program_running = False
+                for player in PLAYERS: #<show> for all players in PLAYERS
+                    for direction, key in zip(player.movements, player.keys): #<show> get all directions (UP, DOWN, LEFT, RIGHT)
+                        if event.key == key: #<show> if new event is player control key
+                            player.movements[direction] = False #<show> set direction to True
+            #End Code Block
+            elif event.type == QUIT: #<show> if new event is quit
+                program_running = False #<show> set the game loop off
+        
+        #GAME LOGIC
+        for player in PLAYERS: #<show> for every player
+            player.update(dt) #<show> update player
+        
+        #DRAWING
+        SCREEN.fill((14, 18, 36)) #<show> fill the screen with the background colour
+        for player in PLAYERS: #<show> for every player
+            SCREEN.blit(player.image, (player.x, player.y)) #<show> Paste the player image at the player coordinate on the SCREEN
+        
+        #SCREEN
+        if HDMode: #<show> if High Definition Mode is True
+            DISPLAY.blit(pygame.transform.smoothscale(SCREEN, WINDOW_SIZE), (0, 0)) #<show> Display Screen on the Window by scaling it to it's resolution 
+        else: #<show> if high Definition Mode is False
+            DISPLAY.blit(pygame.transform.scale(SCREEN, WINDOW_SIZE), (0, 0)) #<show> Display Screen on the Window by smooth scaling it to it's resolution 
+        pygame.display.update() #<show> Update the Screen
+Game()
